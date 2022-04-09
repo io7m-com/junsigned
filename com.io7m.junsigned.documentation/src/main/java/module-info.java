90eb54c2ceba87709b97258eb87408e751fa28e8
Mark Raynsford <code@io7m.com>
2022-04-09 20:12:31 +0000

    Upgrade to 2022 conventions. Update copyright headers. Use OSGi annotations. Require JDK 17.

--- com.io7m.junsigned.documentation/src/main/java/module-info.java
+++ com.io7m.junsigned.documentation/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2015 <code@io7m.com> http://io7m.com
+ * Copyright © 2015 Mark Raynsford <code@io7m.com> https://www.io7m.com
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -21,6 +21,8 @@
 module com.io7m.junsigned.documentation
 {
   requires static org.immutables.value;
+  requires static org.osgi.annotation.bundle;
+  requires static org.osgi.annotation.versioning;
 
   requires com.io7m.jranges.core;
   requires com.io7m.junreachable.core;
--- com.io7m.junsigned.ranges/src/main/java/module-info.java
+++ com.io7m.junsigned.ranges/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2015 <code@io7m.com> http://io7m.com
+ * Copyright © 2015 Mark Raynsford <code@io7m.com> https://www.io7m.com
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -22,6 +22,8 @@ module com.io7m.junsigned.ranges
 {
   requires static com.io7m.immutables.style;
   requires static org.immutables.value;
+  requires static org.osgi.annotation.bundle;
+  requires static org.osgi.annotation.versioning;
 
   requires com.io7m.junreachable.core;
   requires transitive com.io7m.jranges.core;
